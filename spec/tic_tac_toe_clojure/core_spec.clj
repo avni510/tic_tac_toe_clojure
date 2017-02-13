f85fbe23449932c4a656db07fb0610ff82a11990
Avni Kothari <avnikothari@Avnis-MacBook-Air.local>
2017-02-13 13:29:27 -0600

    created project

---
+++ spec/tic_tac_toe_clojure/core_spec.clj
@@ -0,0 +1,7 @@
+(ns tic-tac-toe-clojure.core-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe-clojure.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
